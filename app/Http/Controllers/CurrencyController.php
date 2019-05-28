c129c1fefd730a39a22ff987c4227ada9a8f0923
astr1ke <mail@google.com>
2019-05-28 12:05:21 +0300

    for booking

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -14,10 +14,13 @@ class CurrencyController extends Controller
     public function getEuro() {
         $exFactory = new ExchangeRatesMaker();
 
+        //создаем объект поставщика курса валют
         $exchangeRates = $exFactory->createExchangeRates();
 
+        //пробуем получить курс евро
         $euro = $exchangeRates->getEuroRates();
 
+        //если не удается получить у данного поставщика, переключаемся на следующего
         while ($euro === false || $euro === null) {
             $exchangeRates = $exFactory->nextExchangeRates();
             $euro = $exchangeRates->getEuroRates();
